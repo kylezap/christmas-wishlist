11eec186e91f7a4b8f0ea2e6c77ac943650152f0
kylezap <kzapcic+github@gmail.com>
2025-01-06 12:26:52 -0500

    feat: add dockerfile and application-prod properties file for deploy on render

--- backend/src/main/java/net/christmas_wishlist/demo/config/SecurityConfig.java
+++ backend/src/main/java/net/christmas_wishlist/demo/config/SecurityConfig.java
@@ -47,6 +47,4 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
-
-    
 }

fa684f68fa273fd9a361f0ded3d068b09b81fa17
kylezap <kzapcic+github@gmail.com>
2025-01-06 14:26:51 -0500

    fix: set to env

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+const apiUrl = import.meta.env.VITE_BACKEND_URL;
+
 export default function LoginForm({ setToken }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -7,7 +9,7 @@ export default function LoginForm({ setToken }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:8080/api/auth/login", {
+    const response = await fetch(`${apiUrl}api/auth/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",

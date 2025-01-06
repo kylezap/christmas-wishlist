b8e9c66ed7ac02d8a4032616e2602ba9cdc51eff
kylezap <kzapcic+github@gmail.com>
2025-01-06 12:29:57 -0500

    chore: cleanup code, remove commented out

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -2,10 +2,6 @@ import {
   Disclosure,
   DisclosureButton,
   DisclosurePanel,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
 } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
@@ -19,11 +15,6 @@ const navigation = [
   { name: "My List", href: "#", current: true },
   { name: "Santa", href: "#", current: false },
   { name: "Projects", href: "#", current: false },
-];
-const userNavigation = [
-  { name: "Your Profile", href: "#" },
-  { name: "Settings", href: "#" },
-  { name: "Sign out", href: "#" },
 ];
 
 function classNames(...classes) {
@@ -69,41 +60,15 @@ export default function Nav({ handleLogout, token }) {
                 </div>
               </div>
               <div className="hidden sm:ml-6 sm:flex sm:items-center">
-              {token && (
-      <button
-        type="button"
-        className="rounded bg-red-600 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        onClick={handleLogout}
-      >
-        Logout
-      </button>
-    )}
-
-                {/* Profile dropdown */}
-                {/* <Menu as="div" className="relative ml-3">
-                  <div>
-                    <MenuButton className="relative flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                      <span className="absolute -inset-1.5" />
-                      <span className="sr-only">Open user menu</span>
-                      <img alt="" src={user.imageUrl} className="size-8 rounded-full" />
-                    </MenuButton>
-                  </div>
-                  <MenuItems
-                    transition
-                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+                {token && (
+                  <button
+                    type="button"
+                    className="rounded bg-red-600 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    onClick={handleLogout}
                   >
-                    {userNavigation.map((item) => (
-                      <MenuItem key={item.name}>
-                        <a
-                          href={item.href}
-                          className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
-                        >
-                          {item.name}
-                        </a>
-                      </MenuItem>
-                    ))}
-                  </MenuItems>
-                </Menu> */}
+                    Logout
+                  </button>
+                )}
               </div>
               <div className="-mr-2 flex items-center sm:hidden">
                 {/* Mobile menu button */}
@@ -168,18 +133,6 @@ export default function Nav({ handleLogout, token }) {
                   <BellIcon aria-hidden="true" className="size-6" />
                 </button>
               </div>
-              {/* <div className="mt-3 space-y-1">
-                {userNavigation.map((item) => (
-                  <DisclosureButton
-                    key={item.name}
-                    as="a"
-                    href={item.href}
-                    className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
-                  >
-                    {item.name}
-                  </DisclosureButton>
-                ))}
-              </div> */}
             </div>
           </DisclosurePanel>
         </Disclosure>
--- frontend/src/components/WishList.jsx
+++ frontend/src/components/WishList.jsx
@@ -1,4 +1,3 @@
-
 export default function Wishlist({
   items,
   value,
@@ -11,33 +10,33 @@ export default function Wishlist({
     <div className="flex flex-col justify-center w-2/3 border p-2">
       {/* Input for adding a new wishlist item */}
       <div className="border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
-      <div className="-ml-4 -mt-4 flex flex-wrap items-center justify-between sm:flex-nowrap">
-        <div className="ml-4 mt-4">
-          <h3 className="text-base font-semibold text-gray-900">My List</h3>
-          <p className="mt-1 text-sm text-gray-500">
-            Click the add button to add a new item to your list.
-          </p>
+        <div className="-ml-4 -mt-4 flex flex-wrap items-center justify-between sm:flex-nowrap">
+          <div className="ml-4 mt-4">
+            <h3 className="text-base font-semibold text-gray-900">My List</h3>
+            <p className="mt-1 text-sm text-gray-500">
+              Click the add button to add a new item to your list.
+            </p>
+          </div>
+          <div className="flex justify-center m-4">
+            <input
+              type="text"
+              className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              placeholder="Add a new item"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            />
+          </div>
+          <div className="ml-4 mt-4 shrink-0">
+            <button
+              type="button"
+              className="relative inline-flex items-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={postItem}
+            >
+              Add
+            </button>
+          </div>
         </div>
-      <div className="flex justify-center m-4">
-        <input
-          type="text"
-          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-          placeholder="Add a new item"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
       </div>
-        <div className="ml-4 mt-4 shrink-0">
-          <button
-            type="button"
-            className="relative inline-flex items-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            onClick={postItem}
-          >
-            Add
-          </button>
-        </div>
-      </div>
-    </div>
 
       {/* Mapped list of items */}
       {items.length === 0 ? (
@@ -46,44 +45,52 @@ export default function Wishlist({
         </div>
       ) : (
         <>
-      <ul role="list" className="divide-y divide-gray-100">
-        {items.map((item, index) => (
-          <li key={item.index} className="flex items-center justify-between gap-x-6 py-5">
-            <div className="flex min-w-0 gap-x-4 items-center">
-              <img alt="" src="/favicon.svg" className="size-12 flex-none rounded-full bg-gray-50" />
-              <div className="min-w-0 flex-auto">
-                <p className="text-sm/6 font-semibold text-gray-900">{item.name}</p>
-                {/* <p className="mt-1 truncate text-xs/5 text-gray-500">{person.email}</p> */}
-              </div>
-            </div>
-            <div>
-              {/* TODO: build edit and complete buttons */}
-            <button
-              href="#"
-              className="m-2 rounded-full bg-white px-2.5 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-            >
-              Edit
-            </button>
-            <button
-              href="#"
-              className="m-2 rounded-full bg-white px-2.5 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-              onClick={() => removeItem(index)}
-            >
-                 Remove
-            </button>
-            </div>
-          </li>
-        ))}
-      </ul>
-      <a
-        href="#"
-        className="flex w-full items-center justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0"
-      >
-        Download Wishlist
-      </a>
+          <ul role="list" className="divide-y divide-gray-100">
+            {items.map((item, index) => (
+              <li
+                key={item.index}
+                className="flex items-center justify-between gap-x-6 py-5"
+              >
+                <div className="flex min-w-0 gap-x-4 items-center">
+                  <img
+                    alt=""
+                    src="/favicon.svg"
+                    className="size-12 flex-none rounded-full bg-gray-50"
+                  />
+                  <div className="min-w-0 flex-auto">
+                    <p className="text-sm/6 font-semibold text-gray-900">
+                      {item.name}
+                    </p>
+                    {/* <p className="mt-1 truncate text-xs/5 text-gray-500">{person.email}</p> */}
+                  </div>
+                </div>
+                <div>
+                  {/* TODO: build edit and complete buttons */}
+                  <button
+                    href="#"
+                    className="m-2 rounded-full bg-white px-2.5 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    href="#"
+                    className="m-2 rounded-full bg-white px-2.5 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                    onClick={() => removeItem(index)}
+                  >
+                    Remove
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+          <a
+            href="#"
+            className="flex w-full items-center justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0"
+          >
+            Download Wishlist
+          </a>
         </>
-    )}
-    
+      )}
     </div>
   );
 }

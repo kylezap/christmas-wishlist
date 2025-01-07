ce678731bd077277f74f4fa3e62a94e1cdbb3aa2
kylezap <kzapcic+github@gmail.com>
2025-01-07 16:27:28 -0500

    feat: adding posthog analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { PostHogProvider } from 'posthog-js/react'
+
+const options = {
+  api_host: import.meta.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <PostHogProvider
+      apiKey = {import.meta.env.REACT_APP_PUBLIC_POSTHOG_KEY}
+      options={options}
+      >
+      <App />
+    </PostHogProvider>
+  </StrictMode>
+);
+

dca56a2e30037e5de83b9bc8e3ea5e233a00e84a
kylezap <kzapcic+github@gmail.com>
2025-01-07 12:19:12 -0500

    fix: adjust nav bar so stay consistent. Do not need mobile currently as there aren't enough features

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -14,7 +14,6 @@ const user = {
 const navigation = [
   { name: "My List", href: "#", current: true },
   { name: "Santa", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
 ];
 
 function classNames(...classes) {
@@ -23,120 +22,50 @@ function classNames(...classes) {
 
 export default function Nav({ handleLogout, token }) {
   return (
-    <>
-      <div className="min-h-full">
-        <Disclosure as="nav" className="border-b border-gray-200 bg-white">
-          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <div className="flex h-16 justify-between">
-              <div className="flex">
-                <div className="flex shrink-0 items-center">
-                  <img
-                    alt="Your Company"
-                    src="/favicon.svg"
-                    className="block h-8 w-auto lg:hidden"
-                  />
-                  <img
-                    alt="Your Company"
-                    src="/favicon.svg"
-                    className="hidden h-8 w-auto lg:block"
-                  />
-                </div>
-                <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      aria-current={item.current ? "page" : undefined}
-                      className={classNames(
-                        item.current
-                          ? "border-red-500 text-gray-900"
-                          : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
-                        "inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium"
-                      )}
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
+    <div className="min-h-full">
+      <nav className="border-b border-gray-200 bg-white">
+        <div className="mx-auto max-w-7xl px-4">
+          <div className="flex h-16 justify-between">
+            <div className="flex">
+              <div className="flex shrink-0 items-center">
+                <img
+                  alt="Your Company"
+                  src="/favicon.svg"
+                  className="h-8 w-auto"
+                />
               </div>
-              <div className="hidden sm:ml-6 sm:flex sm:items-center">
-                {token && (
-                  <button
-                    type="button"
-                    className="rounded bg-red-600 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    onClick={handleLogout}
+              <div className="ml-6 flex space-x-8">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    aria-current={item.current ? "page" : undefined}
+                    className={classNames(
+                      item.current
+                        ? "border-red-500 text-gray-900"
+                        : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
+                      "inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium"
+                    )}
                   >
-                    Logout
-                  </button>
-                )}
-              </div>
-              <div className="-mr-2 flex items-center sm:hidden">
-                {/* Mobile menu button */}
-                <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                  <span className="absolute -inset-0.5" />
-                  <span className="sr-only">Open main menu</span>
-                  <Bars3Icon
-                    aria-hidden="true"
-                    className="block size-6 group-data-[open]:hidden"
-                  />
-                  <XMarkIcon
-                    aria-hidden="true"
-                    className="hidden size-6 group-data-[open]:block"
-                  />
-                </DisclosureButton>
+                    {item.name}
+                  </a>
+                ))}
               </div>
             </div>
-          </div>
-
-          <DisclosurePanel className="sm:hidden">
-            <div className="space-y-1 pb-3 pt-2">
-              {navigation.map((item) => (
-                <DisclosureButton
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  aria-current={item.current ? "page" : undefined}
-                  className={classNames(
-                    item.current
-                      ? "border-indigo-500 bg-indigo-50 text-indigo-700"
-                      : "border-transparent text-gray-600 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800",
-                    "block border-l-4 py-2 pl-3 pr-4 text-base font-medium"
-                  )}
-                >
-                  {item.name}
-                </DisclosureButton>
-              ))}
-            </div>
-            <div className="border-t border-gray-200 pb-3 pt-4">
-              <div className="flex items-center px-4">
-                <div className="shrink-0">
-                  <img
-                    alt=""
-                    src={user.imageUrl}
-                    className="size-10 rounded-full"
-                  />
-                </div>
-                <div className="ml-3">
-                  <div className="text-base font-medium text-gray-800">
-                    {user.name}
-                  </div>
-                  <div className="text-sm font-medium text-gray-500">
-                    {user.email}
-                  </div>
-                </div>
+            <div className="ml-6 flex items-center">
+              {token && (
                 <button
                   type="button"
-                  className="relative ml-auto shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  className="rounded bg-red-600 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  onClick={handleLogout}
                 >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon aria-hidden="true" className="size-6" />
+                  Logout
                 </button>
-              </div>
+              )}
             </div>
-          </DisclosurePanel>
-        </Disclosure>
-      </div>
-    </>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 }
